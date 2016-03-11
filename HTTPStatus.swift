a3588b7b170847afd2f54b4f457429c471a7cc97
Tanner Nelson <tanner@bluebite.com>
2016-03-11 17:27:43 -0500

    add source

---
+++ HTTPStatus.swift
@@ -0,0 +1,65 @@
+public enum HTTPStatus {
+    case Continue
+    case SwitchingProtocols
+    case Processing
+
+    case OK
+    case Created
+    case Accepted
+    case NonAuthoritativeInformation
+    case NoContent
+    case ResetContent
+    case PartialContent
+
+    case MultipleChoices
+    case MovedPermanently
+    case Found
+    case SeeOther
+    case NotModified
+    case UseProxy
+    case SwitchProxy
+    case TemporaryRedirect
+    case PermanentRedirect
+
+    case BadRequest
+    case Unauthorized
+    case PaymentRequired
+    case Forbidden
+    case NotFound
+    case MethodNotAllowed
+    case NotAcceptable
+    case ProxyAuthenticationRequired
+    case RequestTimeout
+    case Conflict
+    case Gone
+    case LengthRequired
+    case PreconditionFailed
+    case RequestEntityTooLarge
+    case RequestURITooLong
+    case UnsupportedMediaType
+    case RequestedRangeNotSatisfiable
+    case ExpectationFailed
+    case ImATeapot
+    case AuthenticationTimeout
+    case EnhanceYourCalm
+    case UnprocessableEntity
+    case Locked
+    case FailedDependency
+    case PreconditionRequired
+    case TooManyRequests
+    case RequestHeaderFieldsTooLarge
+
+    case InternalServerError
+    case NotImplemented
+    case BadGateway
+    case ServiceUnavailable
+    case GatewayTimeout
+    case HTTPVersionNotSupported
+    case VariantAlsoNegotiates
+    case InsufficientStorage
+    case LoopDetected
+    case NotExtended
+    case NetworkAuthenticationRequired
+
+    case Raw(statusCode: Int, reasonPhrase: String)
+}
