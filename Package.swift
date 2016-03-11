5660e619b83dda5b6ca1693f0d6a709e378a7a90
Paulo Faria <paulo.faria.rl@gmail.com>
2016-03-11 20:54:31 -0300

    add http version

--- Package.swift
+++ Package.swift
@@ -1,6 +1,5 @@
 import PackageDescription
 
 let package = Package(
-    name: "S4",
-    dependencies: []
+    name: "S4"
 )
---
+++ Sources/HTTPVersion.swift
@@ -0,0 +1,1 @@
+public typealias HTTPVersion = (major: Int, minor: Int)
