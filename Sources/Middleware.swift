00a9824ca3222905406e417ee6a021280e920f93
Paulo Faria <paulo.faria.rl@gmail.com>
2016-03-17 19:47:50 -0300

    add intercept

--- Sources/Middleware.swift
+++ Sources/Middleware.swift
@@ -1,3 +1,23 @@
 public protocol Middleware {
     func respond(request: Request, chain: Responder) throws -> Response
 }
+
+extension Middleware {
+    public func intercept(responder: Responder) -> Responder {
+        return DefaultResponder { request in
+            return try self.respond(request, chain: responder)
+        }
+    }
+}
+
+extension CollectionType where Self.Generator.Element == Middleware {
+    public func intercept(responder: Responder) -> Responder {
+        var responder = responder
+
+        for middleware in self.reverse() {
+            responder = middleware.intercept(responder)
+        }
+
+        return responder
+    }
+}
--- Sources/Responder.swift
+++ Sources/Responder.swift
@@ -1,3 +1,17 @@
 public protocol Responder {
     func respond(request: Request) throws -> Response
 }
+
+typealias Respond = Request throws -> Response
+
+struct DefaultResponder: Responder {
+    let respond: Respond
+
+    init(_ respond: Respond) {
+        self.respond = respond
+    }
+
+    func respond(request: Request) throws -> Response {
+        return try respond(request)
+    }
+}
