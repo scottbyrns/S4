566ee4eb692bbec990e4e1ef3665d397d10cba64
Paulo Faria <paulo.faria.rl@gmail.com>
2016-03-21 00:30:58 -0300

    add request/response initializers

--- Sources/Request.swift
+++ Sources/Request.swift
@@ -5,4 +5,13 @@ public struct Request: Message {
     public var headers: Headers
     public var body: Body
     public var storage: Storage
+
+    public init(method: Method, uri: URI, version: Version, headers: Headers, body: Body, storage: Storage) {
+        self.method = method
+        self.uri = uri
+        self.version = version
+        self.headers = headers
+        self.body = body
+        self.storage = storage
+    }
 }
--- Sources/Response.swift
+++ Sources/Response.swift
@@ -4,4 +4,12 @@ public struct Response: Message {
     public var headers: Headers
     public var body: Body
     public var storage: Storage
+
+    public init(version: Version, status: Status, headers: Headers, body: Body, storage: Storage) {
+        self.version = version
+        self.status = status
+        self.headers = headers
+        self.body = body
+        self.storage = storage
+    }
 }
