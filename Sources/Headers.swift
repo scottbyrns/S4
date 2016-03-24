f87877eb52453f59827f171b8d6b3c55dbcc00bf
Paulo Faria <paulo.faria.rl@gmail.com>
2016-03-24 18:42:15 -0300

    make headers a struct

--- Sources/Headers.swift
+++ Sources/Headers.swift
@@ -1,1 +1,49 @@
-public typealias Headers = [Header]
+public struct Headers {
+    public var headers: [HeaderName: HeaderValues]
+
+    public init(_ headers: [HeaderName: HeaderValues]) {
+        self.headers = headers
+    }
+}
+
+extension Headers: DictionaryLiteralConvertible {
+    public init(dictionaryLiteral elements: (HeaderName, HeaderValues)...) {
+        var headers: [HeaderName: HeaderValues] = [:]
+
+        for (key, value) in elements {
+            headers[key] = value
+        }
+
+        self.headers = headers
+    }
+}
+
+extension Headers: NilLiteralConvertible {
+    public init(nilLiteral: ()) {
+        self.headers = [:]
+    }
+}
+
+extension Headers: Sequence {
+    public func makeIterator() -> DictionaryIterator<HeaderName, HeaderValues> {
+        return headers.makeIterator()
+    }
+
+    public var count: Int {
+        return headers.count
+    }
+
+    public var isEmpty: Bool {
+        return headers.isEmpty
+    }
+
+    public subscript(name: HeaderName) -> HeaderValues {
+        get {
+            return headers[name] ?? nil
+        }
+
+        set(headerValues) {
+            headers[name] = headerValues
+        }
+    }
+}
